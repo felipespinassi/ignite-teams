e74ca2db9f09d08d8c0994a8791fe16ba07963a4
felipespinassi <felipe_spinassi@hotmail.com>
2023-01-22 17:02:39 -0300

    criando componente de loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme/index";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,10 @@
+import { Text } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Text>opa</Text>
+    </Container>
+  );
 }
